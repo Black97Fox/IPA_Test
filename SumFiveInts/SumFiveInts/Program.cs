9b659d913c2439dd61e6e7e1571c88843cd75954
Black97Fox <43849373+Black97Fox@users.noreply.github.com>
2020-02-23 14:03:04 +0300

    While Test1-Fix!

---
+++ SumFiveInts/SumFiveInts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumFiveInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int number = 0;
+            int sum = 0;
+            while(number != 999)
+            {
+                Console.WriteLine("Insert No:" + count);
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number != 999)
+                {
+                    sum = sum + number;
+                }
+                count = count + 1;
+            }
+            Console.WriteLine("The Sum is:" + sum);
+        }
+    }
+}
